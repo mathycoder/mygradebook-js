df9e170a1402ff0fd02b6e9e9a5d88d88a976ff2
Adam Sarli <adam.sarli@gmail.com>
2019-09-04 13:55:23 -0400

    getData() returns JSON and creates Grade objects

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -9,18 +9,11 @@ $(document).on('turbolinks:load', function() {
 
 function getData() {
   const klassId = window.location.href.split("/")[4]
-  $.get('/classes/' + klassId + '.json', function(json){
-    // Serializer grabs way too many grades... not sure why
-    console.log(json)
-    createJSONObjects(json.learning_targets, LearningTarget)
-    createJSONObjects(json.standards, Standard)
-    createJSONObjects(json.students, Student)
-    createJSONObjects(json.assignments, Assignment)
-    createJSONObjects(json.grades, Grade)
+  $.get('/classes/' + klassId + '/grades', function(json){
+    createJSONObjects(json, Grade)
   })
 }
 
-// Make these more efficient...one function!!
 function createJSONObjects(json, cla){
   for (i = 0; i<json.length; i++){
     new cla(json[i])
