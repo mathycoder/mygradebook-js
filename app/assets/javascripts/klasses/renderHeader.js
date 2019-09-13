8cdff9bf38f6545d15b9aa6105b6718ad2565082
Adam Sarli <adam.sarli@gmail.com>
2019-09-12 21:33:19 -0400

    header functional

--- app/assets/javascripts/klasses/renderHeader.js
+++ app/assets/javascripts/klasses/renderHeader.js
@@ -1,30 +1,3 @@
-$().ready(() => {
-  if (/^http:\/\/localhost:3000\/classes$/.test(window.location.href)){
-    getIndexData()
-  }
-})
-
-function getIndexData(){
-  $('main')[0].innerHTML = ''
-  $.get(`/classes.json`, function(json){
-    for (i = 0; i<json.length; i++){
-      klasses.push(new Klass(json[i]))
-    }
-    let indexHtml = Klass.formatIndex(json[0].teachers[0].name)
-    $('main').append(indexHtml)
-    $('.class-link').click(clickOnClass)
-  })
-}
-
-function clickOnClass(e){
-  e.preventDefault()
-  const klassId = this.dataset.id
-  $('main')[0].innerHTML = ''
-  renderKlassShowHeader(klassId)
-  getData(klassId)
-  $('.class-header select').change(switchClass)
-}
-
 function renderKlassShowHeader(klassId){
   $('header')[0].innerHTML = ''
   let html = ''
