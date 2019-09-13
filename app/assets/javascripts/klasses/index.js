6ae623cfc7deaf6abc43653c7492ae963e5a2d04
Adam Sarli <adam.sarli@gmail.com>
2019-09-12 20:50:40 -0400

    rendering header in JS; first 3 buttons done

--- app/assets/javascripts/klasses/index.js
+++ app/assets/javascripts/klasses/index.js
@@ -21,5 +21,40 @@ function clickOnClass(e){
   const klassId = this.dataset.id
   $('main')[0].innerHTML = ''
   clearData()
+  modifyHeader(klassId)
   getData(klassId)
+}
+
+function modifyHeader(klassId){
+  $('header')[0].innerHTML = ''
+  let html = ''
+
+  html += `
+    <a href="http://localhost:3000/classes/${klassId}">
+      <div class="header-logo">
+        <img src="/assets/open-book2.png">
+      </div>
+    </a>
+    <div>
+      <form action="http://localhost:3000/classes/${klassId}/assignments/new" method="get">
+        <input name="utf8" type="hidden" value="✓">
+        <input type="submit" name="commit" value="+A" class="fancy-button" data-disable-with="+A">
+      </form>
+    </div>
+    <div>
+      <form action="http://localhost:3000/classes/${klassId}/lts/new" method="get">
+        <input name="utf8" type="hidden" value="✓">
+        <input type="submit" name="commit" value="+LT" class="fancy-button" data-disable-with="+LT">
+      </form>
+    </div>
+    <div>
+      <form action="http://localhost:3000/classes/${klassId}/students" method="get">
+        <input name="utf8" type="hidden" value="✓">
+        <input type="submit" name="commit" value="+S" class="fancy-button" data-disable-with="+S">
+      </form>
+    </div>
+
+  `
+
+  $('header').append(html)
 }
