48e6ddffd5584bfdcdb96e0ac87fbbe630857e46
Adam Sarli <adam.sarli@gmail.com>
2019-09-19 16:48:48 -0400

    students/index.js now allows adding and removing students from the DOM

--- app/assets/javascripts/students/index.js
+++ app/assets/javascripts/students/index.js
@@ -20,7 +20,7 @@ function createStudentsInSchool(json, cla){
     let student = new cla(json[i])
     $('#students-in-school tbody').append(student.fullTrInSchoolHTML())
   }
-  $('.add-student-button').click(changeKlassStatus)
+  $('.add-student-button').click(addStudentToKlass)
 }
 
 function createStudentsInKlass(json, cla){
@@ -28,7 +28,7 @@ function createStudentsInKlass(json, cla){
     let student = new cla(json[i])
     $('#students-in-klass tbody').append(student.fullTrInKlassHTML())
   }
-  $('.remove-student-button').click(changeKlassStatus)
+  $('.remove-student-button').click(removeStudentFromKlass)
 }
 
 function filterIndexStudents(e){
@@ -46,17 +46,35 @@ function filterIndexStudents(e){
   })
 }
 
-function changeKlassStatus(){
+function addStudentToKlass(){
+  const element = this
+  $(this).parent().parent().remove()
+  changeKlassStatus(element)
+}
+
+function removeStudentFromKlass(){
+  const element = this
+  $(this).parent().parent().remove()
+  changeKlassStatus(element)
+}
+
+function changeKlassStatus(element){
   const klassId = window.location.href.split("/")[4]
-  const studentId = this.id.split("-")[1]
+  const studentId = element.id.split("-")[1]
    $.ajax({
    type: 'PATCH',
    url: `/classes/${klassId}/students/${studentId}`
  }).done((data, status, xhr) => {
    if (xhr.status === 201){
      console.log("added student!")
+     const student = new Student(data)
+     $('#students-in-klass tr:first-child').after(student.fullTrInKlassHTML())
+     $(`#student-${student.id} .remove-student-button`).click(removeStudentFromKlass)
    } else if (xhr.status === 200){
      console.log("removed student!")
+     const student = new Student(data)
+     $('#students-in-school tr:first-child').after(student.fullTrInSchoolHTML())
+     $(`#student-${student.id} .add-student-button`).click(addStudentToKlass)
    }
  })
 }
